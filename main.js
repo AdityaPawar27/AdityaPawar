cd581f788a00dec60d948e03ac16fa0618df578f
AdityaPawar27 <64799729+AdityaPawar27@users.noreply.github.com>
2020-06-24 18:20:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-$(function() {
-  // Add the hover() method for #language-wrapper
-  $('#Dropdown').hover(
-    function(){
-      $('.header-list').fadeIn(300);
-    },
-    function(){
-      $('.header-list').fadeOut(100);
-    }
+$(document).ready(function () {
+	var previousScroll = 0;
+	$(window).scroll(function () {
+		var currentScroll = $(this).scrollTop();
+		if (currentScroll < 100) {
+			showTopNav();
+		} else if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
+			if (currentScroll > previousScroll) {
+				hideNav();
+			} else {
+				showNav();
+			}
+			previousScroll = currentScroll;
+		}
+	});
 
-);
+	function hideNav() {
+		$(".navbar").removeClass("is-visible").addClass("is-hidden");
+	}
+
+	function showNav() {
+		$(".navbar").removeClass("is-hidden").addClass("is-visible").addClass("scrolling");
+	}
 });
